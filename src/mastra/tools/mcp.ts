caf8634e30c4ab2d03936737b928db60af894bca
Yujohn Nattrass <yujohn.natt@gmail.com>
2025-08-05 15:47:59 -0700

    remove github mcp

--- src/mastra/tools/mcp.ts
+++ src/mastra/tools/mcp.ts
@@ -2,14 +2,6 @@ import { MCPClient } from "@mastra/mcp";
 
 const mcp = new MCPClient({
     servers: {
-        github: {
-            url: new URL("https://api.githubcopilot.com/mcp"),
-            requestInit: {
-                headers: {
-                    Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
-                },
-            },
-        },
         hackernews: {
             command: "npx",
             args: ["-y", "@devabdultech/hn-mcp-server"],
